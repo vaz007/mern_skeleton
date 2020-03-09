1ffcb6d59ae6903dfbddeecf43ca155415a06d18
vaz007 <ankur@adaptideations.com>
2020-03-10 00:07:18 +1100

    initial backend skeleton commit

---
+++ Backend_Crud_JWT/Readme.md
@@ -0,0 +1,15 @@
+Basic skeleton of mern stack. 
+
+To run the application locally just clone the git repo
+
+run npm -i to install all the dependencies 
+
+and then run the following command
+
+npm run development 
+
+
+PRODUCTION LEVEL
+npm run build will generate client and server code bundles
+Remove devbundle.compile from server.js before prduction mode
+npm run start will run bundled code for production
---
+++ Backend_Crud_JWT/server/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import devbundle from "./devBundle";
+import path from "path";
+import { MongoClient } from "mongodb";
+
+import template from "./../template";
+
+const app = express();
+
+// npm run build will generate client and server code bundles
+// Remove devbundle.compile before prduction mode
+// npm run start will run bundled code for production
+devbundle.compile(app);
+
+const CURRENT_WORKING_DIR = process.cwd();
+app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+
+app.get("/", (req, res) => {
+  res.status(200).send(template());
+});
+
+const url =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/mernSimpleSetup";
+MongoClient.connect(url, (err, db) => {
+  console.log("Connected successfully to mongodb server");
+  db.close();
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Server started on port %s.", port);
+});
